0b9c937ce069b77d86ac0023dab1a46499e3912e
Pedro Duarte <pedro.duarte@tecnico.ulisboa.pt>
2015-07-19 23:26:20 +0100

    Removed logic for Apple iTunes store in favor of the apple-store-link module.

--- index.js
+++ index.js
@@ -1,46 +1,53 @@
 'use strict';
-var empty = require('is-empty');
-var inArray = require('in-array');
-var trim = require('trim');
+var appleStoreLink = require('apple-store-link');
+var empty          = require('is-empty');
+var inArray        = require('in-array');
+var trim           = require('trim');
 
-module.exports = function (store, id, lang) {
-	// Default value for the language argument.
-	lang = typeof lang === 'string' ? a : 'us';
+module.exports = function (store, id, lang, developer, protocol) {
 
 	// Check if either input variable is of type string.
 	if (typeof store !== 'string') {
 		throw new TypeError('Expected a string for store.');
+	} else {
+		store = trim(store);
 	}
 	if (typeof id !== 'string') {
 		throw new TypeError('Expected a string for id.');
 	}
 
-	// Remove leading/trailing white space.
-	store = trim(store);
-	id = trim(id);
-	lang = trim(lang);
-
-	var stores = ['ios', 'android', 'windows'];
+	// Handle optional inputs.
+	if (typeof lang !== 'string') {
+		lang = null;
+	} else {
+		lang = trim(lang);
+	}
+	if (typeof developer !== 'string') {
+		developer = null;
+	} else {
+		developer = trim(developer);
+	}
+	if (typeof protocol !== 'string') {
+		protocol = null;
+	} else {
+		protocol = trim(protocol);
+	}
 
 	// Check if store is supported (i.e. is present in the stores array).
-	if(!inArray(stores, store)) {
+	var stores = ['ios', 'android', 'windows'];
+
+	if( !inArray(stores, store) ) {
 		throw new TypeError('Store misspelled or not supported.');
 	}
 
 	var baseLinks = {
-		'ios': {
-			'pre-locale': 'https://itunes.apple.com/',
-			'pre-id': '/app/id'
-		},
 		'android': 'http://play.google.com/store/apps/details?id=',
 		'windows': 'http://www.windowsphone.com/s?appid='
 	}
 
 	if(store === 'ios') {
-		var link = baseLinks['ios']['pre-locale'] + lang + baseLinks['ios']['pre-id'];
+		return appleStoreLink(id, lang, developer, protocol);
 	} else {
-		var link = baseLinks[store];
+		return baseLinks[store] + id;
 	}
-
-	return link + id;
 };
--- test.js
+++ test.js
@@ -1,11 +1,19 @@
 'use strict';
-var assert = require('assert');
+var assert    = require('assert');
 var storeLink = require('./');
-var id = 'myapp';
+var id        = 'myapp';
+var lang = 'us';
+var developer = 'ted';
 
+// Test Apple iTunes link construction.
 it('should ', function () {
-	// Test link construction.
-	assert.strictEqual(storeLink('ios', id), 'https://itunes.apple.com/us/app/id' + id);
+	assert.strictEqual(storeLink('ios', id), 'https://itunes.apple.com/app/id' + id);
+	assert.strictEqual(storeLink('ios', id, lang), 'https://itunes.apple.com/' + lang + '/app/id' + id);
+	assert.strictEqual(storeLink('ios', id, null, developer), 'https://itunes.apple.com/app/' + developer + '/id' + id);
+});
+
+// Test Google Play and Windows Stores link construction.
+it('should ', function () {
 	assert.strictEqual(storeLink('android', id), 'http://play.google.com/store/apps/details?id=' + id);
 	assert.strictEqual(storeLink('windows', id), 'http://www.windowsphone.com/s?appid=' + id);
 });
